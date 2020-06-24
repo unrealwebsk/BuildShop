c92d02b716fa9ebb47a4c24372383373b402c155
unrealwebsk <unrealwebsk@gmail.com>
2020-06-24 17:06:58 +0200

    another commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 const scrollBtns = document.querySelectorAll('.scroll-to');
 
 scrollBtns.forEach(btn => {
+    hideMobileMenu(btn)
    btn.addEventListener('click', (e) => {
       e.preventDefault();
 
@@ -24,3 +25,36 @@ window.addEventListener('scroll', (e) => {
     scrollToTop.classList.remove('active')
   }
 });
+
+// Toggle mobile menu
+
+const burger         = document.querySelector('#burger');
+const close          = document.querySelector('#close-menu');
+const menu           = document.querySelector('#mobile-menu');
+const body           = document.querySelector('body');
+const overlay        = document.querySelector('#overlay');
+
+showMobileMenu(burger);
+hideMobileMenu(close);
+
+// Functions
+
+function showMobileMenu(trigger) {
+  trigger.addEventListener('click', (e)=> {
+    e.preventDefault();
+  
+    overlay.classList.remove('hidden');
+    body.classList.add('overflow-hidden');
+    menu.classList.add('active');
+  })
+}
+
+function hideMobileMenu(trigger) {
+  trigger.addEventListener('click', (e)=> {
+    e.preventDefault();
+  
+    body.classList.remove('overflow-hidden');
+    menu.classList.remove('active');
+    overlay.classList.add('hidden');
+  })
+}
